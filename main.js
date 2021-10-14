c4abc75f410bcbc183451d2e159c93e4e9ad337b
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-10-13 18:51:38 -0700

    Project

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
 
 function setup() {
     canvas = createCanvas(600, 500);
@@ -6,10 +11,27 @@ function setup() {
 
     video = createCature(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function draw(){
-    image(video, 0, 0, 600, 500);
+function gotPoses(results){
+    if(results.lenth > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        console.log("leftWristX = "+ leftWristX + "leftWristY = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+ rightWristX + "rightWristY = "+rightWristY);
+    }
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
 }
 
 function preload(){
